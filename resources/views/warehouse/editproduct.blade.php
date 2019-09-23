9957fbe28869d5ec59333b7627762a5d1a56c91e
orbachinujbuk <orbachinujbuk@gmail.com>
2019-09-23 08:18:33 +0600

    dfggggg

--- resources/views/warehouse/editproduct.blade.php
+++ resources/views/warehouse/editproduct.blade.php
@@ -109,29 +109,29 @@
   		      </div>
   		      <div class="col-md-3">
   		        {!! Form::label('carrying_cost', 'Carrying Cost (%) *') !!}
-  		        {!! Form::text('carrying_cost', 0, array('class' => 'form-control', 'required' => '')) !!}
+  		        {!! Form::text('carrying_cost', null, array('class' => 'form-control', 'required' => '')) !!}
   		      </div>
   		      <div class="col-md-3">
   		        {!! Form::label('vat', 'Vat (%) *') !!}
-  		        {!! Form::text('vat', 0, array('class' => 'form-control', 'required' => '')) !!}
+  		        {!! Form::text('vat', null, array('class' => 'form-control', 'required' => '')) !!}
   		      </div>
   		      <div class="col-md-3">
   		        {!! Form::label('salary', 'Salary (%) *') !!}
-  		        {!! Form::text('salary', 0, array('class' => 'form-control', 'required' => '')) !!}
+  		        {!! Form::text('salary', null, array('class' => 'form-control', 'required' => '')) !!}
   		      </div>
   		    </div>
   		    <div class="row">
   		      <div class="col-md-3">
   		        {!! Form::label('wages', 'Wages (%) *') !!}
-  		        {!! Form::text('wages', 0, array('class' => 'form-control', 'required' => '')) !!}
+  		        {!! Form::text('wages', null, array('class' => 'form-control', 'required' => '')) !!}
   		      </div>
   		      <div class="col-md-3">
   		        {!! Form::label('utility', 'Utility (%) *') !!}
-  		        {!! Form::text('utility', 0, array('class' => 'form-control', 'required' => '')) !!}
+  		        {!! Form::text('utility', null, array('class' => 'form-control', 'required' => '')) !!}
   		      </div>
   		      <div class="col-md-3">
   		        {!! Form::label('others', 'Others (%) *') !!}
-  		        {!! Form::text('others', 0, array('class' => 'form-control', 'required' => '')) !!}
+  		        {!! Form::text('others', null, array('class' => 'form-control', 'required' => '')) !!}
   		      </div>
   		    </div>
   		  </div>
