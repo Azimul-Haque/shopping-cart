8d4d8fbae10d5567787cd12eabb6e3c81bc5b4c9
orbachinujbuk <orbachinujbuk@gmail.com>
2019-09-25 05:36:00 +0600

    asd asd

--- resources/views/shop/singleproduct.blade.php
+++ resources/views/shop/singleproduct.blade.php
@@ -20,7 +20,7 @@
   </style>
   <meta property="og:image" content="{{ asset('images/product-images/'.$product->productimages->first()->image) }}" />
   <meta property="og:title" content="{{ $product->title }} | Ecoomerce Name"/>
-  <meta name="description" property="og:description" content="{{ substr(strip_tags($product->description), 0, 200) }}" />
+  <meta name="description" property="og:description" content="৳ {{ $product->price }} | {{ substr(strip_tags($product->description), 0, 200) }}" />
   <meta property="og:type" content="article"/>
   <meta property="og:url" content="{{ Request::url() }}" />
   <meta property="og:site_name" content="Ecomm Name">
